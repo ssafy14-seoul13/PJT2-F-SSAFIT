7e2ea5c206fea5e0373484f4ae73cfa14f7d22f8
TerryAhn <t3rryahn.dev@gmail.com>
2025-09-05 16:08:36 +0900

    feat: header, footer 모듈화

---
+++ ssafit/frontend/js/layout.js
@@ -0,0 +1,12 @@
+function loadHTML(id, file) {
+  fetch(file)
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById(id).innerHTML = html;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadHTML("header-container", "/ssafit/frontend/partials/header.html");
+  loadHTML("footer-container", "/ssafit/frontend/partials/footer.html");
+});
